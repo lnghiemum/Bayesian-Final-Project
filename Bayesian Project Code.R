933c2fbcf50031f8bb14d2b86d6147b18ea66242
lnghiemum <l.nghiemum@gmail.cm>
2016-12-02 20:27:40 -0600

    Updated 1202

--- Bayesian Project Code.R
+++ Bayesian Project Code.R
@@ -30,3 +30,60 @@ for (k in 1:length(beta)){
 }
 
 theta
+
+# LDA implementation
+
+# Generative process
+# dictionary
+dict <- c("a", "b", "c", "d", "e")
+# 3 topics possible K = 3
+beta = rep(1,5) # distribution over words
+library(MCMCpack) #For Dirichlet distribution
+phi = rdirichlet(3,beta)
+# Document 1
+alpha = rep(1,3)
+theta = rdirichlet(1,alpha)
+# Each single word
+z= numeric(1000) # topic index assignment
+w=character(1000)
+for (i in 1:1000){
+  z[i]=sample(1:3, 1, prob=theta)
+  w[i]=dict[sample(1:5, 1, prob=phi[z[i],])]
+}
+z
+w
+# Now given d, use Gibbs sampling
+K=3
+alpha=rep(1,K)
+beta=rep(1,5) # Number of words in dictionary
+z.t = sample(1:K,1000, replace=TRUE) # Initial topic assignment
+# One document, so n_d,k and n_k are the same
+n_k = numeric(K)
+for (i in 1:K){
+  n_k[i] = length(z.t[z.t==i])
+}
+n_kw = matrix(0, nrow=1000, ncol=K) # Each row is a word, each column is a topic. Count how many times a word is assigned to a topic
+for (i in 1:1000){ 
+  n_kw[i,z.t[i]]=1
+}
+n_kw[1:3,]
+for(iteration in 1:200){
+  print(iteration)
+  for (word in 1:1000){
+    topic <- z.t[word]
+    #n_k[topic] = n_k[topic]-1
+    #n_kw[word,topic] = n_kw[word,topic]-1
+    prob=numeric(3)
+    for (k in 1:3){
+      prob[k]=(n_k[k] + alpha[k])*(n_kw[word,topic]+beta[which(dict==w[word])])/(n_k[k]+sum(beta))
+    }
+    topic = sample(1:K,1,replace=TRUE, prob=prob)
+    z.t[word]= topic
+    n_k[topic] = n_k[topic]+1
+    n_kw[word,topic] = n_kw[word,topic]+1
+  }
+}
+
+
+
+
